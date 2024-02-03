c1f4b6d78ac8f1fe871832f3b0c86c7e965abfe2
m4rkbello <markbello10041999@gmail.com>
2024-02-03 20:44:11 +0800

    FIXED LOGIN FORM WITH VALIDATIONS

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,12 +47,15 @@
 
 
     <div class="hero min-h-screen bg-base-200">
+    
         <div class="hero-content flex-col lg:flex-row-reverse">
             <div class="text-center lg:text-left">
                 <h1 class="text-5xl font-bold">Login now!</h1>
-                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <p class="py-6">MARK BELLO</p>
             </div>
+            
             <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                
                 <form method="POST" action="{{ route('login') }}" class="card-body">
                     @csrf
                     <!-- Email Address -->
